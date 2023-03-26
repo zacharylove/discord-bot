0b811a78b568b5a457e35a68f2c56d73121f968c
Zach Love <abc811@gmail.com>
2023-03-26 15:16:08 -0400

    Add test GIF command

---
+++ src/commands/animAvatarTest.ts
@@ -0,0 +1,164 @@
+import { CommandInterface, CommandProperties } from '../interfaces/Command';
+import { EmbedBuilder, SlashCommandBuilder } from "@discordjs/builders";
+import { getAvatarURL } from '../utils/userUtils';
+import { decompressFrames, ParsedFrame, parseGIF } from 'gifuct-js';
+import { createCanvas, ImageData, loadImage } from 'canvas';
+import path from 'path';
+
+
+
+export const animAvatarTest: CommandInterface = {
+    data: new SlashCommandBuilder()
+        .setName('animavatartest')
+        .setDescription("Test animation")
+        .addUserOption((option) =>
+            option
+                .setName('user')
+                .setDescription('The user to get the avatar for')
+                .setRequired(true)
+        ),
+    run: async (interaction) => {
+        await interaction.deferReply();
+        let user = interaction.options.getUser('user');
+        if (!user || !interaction.guild) {
+            return;
+        }
+        const avatar = await getAvatarURL(user, interaction.guild.id);
+        const avatarType = avatar[0];
+        const avatarURL = avatar[1];
+
+        if (avatarType != "gif") { return; }
+
+
+
+        // Gif frame data
+        let frameImageData: ImageData;
+        let frameIndex: number = 0;
+        let loadedFrames: ParsedFrame[];
+        let needsDisposal: boolean = false;
+        
+        // Encoder to build output gif
+        const GIFencoder = require('gif-encoder-2');
+        
+
+        // Fetch gif and split into array of all GIF image frames and metadata
+        loadedFrames = await fetch(avatarURL)
+        .then(resp => resp.arrayBuffer())
+        .then(buff => {
+            var gif = parseGIF(buff);
+            var frames = decompressFrames(gif, true);
+            return frames;
+        });
+
+        // Sample first frame to get dimensions
+        const firstFrame = loadedFrames[0];
+
+        const canvasWidth = 360;
+        const canvasHeight = 403;
+        const gifWidth = firstFrame.dims.width;
+        const gifHeight = firstFrame.dims.height;
+
+        // Full gif canvas
+        const outputCanvas = createCanvas(canvasWidth, canvasHeight);
+        const outputCtx = outputCanvas.getContext('2d');
+
+        // Gif patch canvas
+        const gifPatchCanvas = createCanvas(gifWidth, gifHeight);
+        const gifPatchCtx = gifPatchCanvas.getContext('2d');
+
+        // Gif encoder
+        const encoder = new GIFencoder(canvasWidth, canvasHeight, "neuquant", true);
+        encoder.setThreshold(60);
+        encoder.setDelay(firstFrame.delay);
+        encoder.start();
+
+        // Load overlay
+        const overlay = await loadImage(path.join(__dirname, '..', '..', 'assets', 'img', 'beautiful.png'));
+
+        // Render gif
+        renderGif(loadedFrames);
+
+        
+
+        // Draws frame data over canvas
+        function drawPatch(frame: ParsedFrame) {
+            var dims = frame.dims;
+            // Set frame image to size of gif frame
+            if ( !frameImageData ||
+                dims.width != frameImageData.width ||
+                dims.height != frameImageData.height ) {
+                    gifPatchCanvas.width = dims.width;
+                    gifPatchCanvas.height = dims.height;
+                    frameImageData = gifPatchCtx.createImageData(dims.width, dims.height);
+            }
+            frameImageData.data.set(frame.patch);
+            gifPatchCtx.putImageData(frameImageData, 0, 0);
+            outputCtx.drawImage(gifPatchCanvas, 237, 25, 100, 100);
+            outputCtx.drawImage(gifPatchCanvas, 237, 230, 100, 100);
+            
+        }
+
+        // Entry point for rendering gif
+        function renderGif(frames: ParsedFrame[] ) {
+            loadedFrames = frames;
+            frameIndex = 0;
+            //canvas.width = frames[0].dims.width;
+            //canvas.height = frames[0].dims.height;
+            outputCanvas.width = canvasWidth;
+            outputCanvas.height = canvasHeight;
+
+            
+            renderFrame();
+        }
+
+        // Recursively called to render each frame
+        function renderFrame() {
+            let frame = loadedFrames[frameIndex];
+
+            if (needsDisposal) {
+                gifPatchCtx.clearRect(0, 0, gifPatchCanvas.width, gifPatchCanvas.height);
+                needsDisposal = false;
+            }
+
+            // draw patch
+            drawPatch(frame);
+            // update frame index
+            frameIndex++;
+            if (frameIndex >= loadedFrames.length) { return }
+            // update disposal
+            if (frame.disposalType === 2) needsDisposal = true;
+
+            // Add overlay
+            outputCtx.drawImage(overlay, 0, 0, canvasWidth, canvasHeight)
+            //outputCtx.font = "30px Impact";
+            //outputCtx.fillText("I want to", 0, frame.dims.height / 4, frame.dims.width);
+            //outputCtx.fillText("die", frame.dims.width / 3, frame.dims.height, frame.dims.width);
+
+
+            // Add frame to gif
+            if (outputCtx == null) {
+                throw new Error("Output context is null");
+            }
+            encoder.addFrame(outputCtx);
+
+            renderFrame();
+        }
+        
+        // Finish writing gif
+        encoder.finish();
+        const buffer = encoder.out.getData();
+
+        interaction.editReply(
+            { files: [{attachment: buffer, name: 'test.gif'}]}
+        );
+
+    },
+    properties: {
+        Name: 'AnimAvatarTest',
+        Scope: 'global',
+        GuildOnly: false,
+        Enabled: true,
+        DefaultEnabled: false,
+        Intents: []
+    }
+}
